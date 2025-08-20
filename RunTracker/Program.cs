137d0a07653ba9527eec99822b173db528d0f1c9
Bradley H. <111481356+bheston1@users.noreply.github.com>
2025-08-20 08:40:21 -0400

    Add project files.

---
+++ RunTracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RunTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
