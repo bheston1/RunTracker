02644310267c3a8033724f9a7660fe072e07c50e
Bradley H. <111481356+bheston1@users.noreply.github.com>
2025-08-20 09:04:26 -0400

    add Database initializer

---
+++ RunTracker/Database.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracker
+{
+    public static class Database
+    {
+        public static string ConnectionString = ConfigurationManager.AppSettings.Get("SQLite Connection String");
+
+        public static void Initialize()
+        {
+            using SqliteConnection connection = new SqliteConnection(ConnectionString);
+            var sql = "CREATE TABLE IF NOT EXISTS RunSession (Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, StartTime TEXT, EndTime TEXT, Miles REAL)";
+            connection.Execute(sql);
+        }
+    }
+}
