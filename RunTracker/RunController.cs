54c66882e18d6f77cc0cf566bfb1f0c9f90731e6
Bradley H. <111481356+bheston1@users.noreply.github.com>
2025-08-20 11:03:32 -0400

    add RunSession model and database controller

---
+++ RunTracker/RunController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracker
+{
+    public static class RunController
+    {
+        public static void AddSession(DateTime date, DateTime startTime, DateTime endTime, double miles)
+        {
+            using (var connection = new SqliteConnection(Database.ConnectionString))
+            {
+                var sql = "INSERT INTO RunSessions (Date, StartTime, EndTime, Miles) VALUES (@Date, @StartTime, @EndTime, @Miles)";
+                connection.Execute(sql, new
+                {
+                    Date = date.ToString("M/d/yyyy"),
+                    StartTime = startTime.ToString("h:m tt"),
+                    EndTime = endTime.ToString("h:m tt"),
+                    Miles = miles
+                });
+            }
+        }
+
+        public static List<RunSession> GetSessions()
+        {
+            var runSessions = new List<RunSession>();
+
+            // do shit
+
+            return runSessions;
+        }
+    }
+}
---
+++ RunTracker/RunSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracker
+{
+    public class RunSession
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Miles { get; set; }
+    }
+}
