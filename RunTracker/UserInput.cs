75b7c53b86c20d0999d6475469758c6730a26d8f
Bradley H. <111481356+bheston1@users.noreply.github.com>
2025-08-20 11:40:52 -0400

    add UserInput class and replace Menu class with UserInterface

---
+++ RunTracker/UserInput.cs
@@ -0,0 +1,33 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracker
+{
+    public static class UserInput
+    {
+        public static DateTime GetDateTime(string prompt, string format)
+        {
+            DateTime dateTime;
+
+            while (true)
+            {
+                var input = AnsiConsole.Ask<string>(prompt);
+
+                if (!DateTime.TryParseExact(input, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dateTime))
+                {
+                    AnsiConsole.Markup("[yellow]Invalid format[/]\n");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return dateTime;
+        }
+    }
+}
---
+++ RunTracker/UserInterface.cs
@@ -0,0 +1,64 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracker
+{
+    public static class UserInterface
+    {
+        public static void MainMenu()
+        {
+            string[] choices =
+            {
+                "Add new record",
+                "View all records",
+                "Update existing record",
+                "Delete a record",
+                "Close app"
+            };
+
+            while (true)
+            {
+                Console.Clear();
+
+                var selection = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Select option").AddChoices(choices));
+
+                switch (selection)
+                {
+                    case "Add new record":
+                        AddRecord();
+                        break;
+
+                    case "View all records":
+
+                        break;
+
+                    case "Update existing record":
+
+                        break;
+
+                    case "Delete a record":
+
+                        break;
+
+                    case "Close app":
+                        return;
+                }
+            }
+        }
+
+        private static void AddRecord()
+        {
+            var date = UserInput.GetDateTime("Enter session date (m/d/yyyy): ", "M/d/yyyy");
+            var start = UserInput.GetDateTime("Enter start time (h:m am/pm): ", "h:m tt");
+            var end = UserInput.GetDateTime("Enter start time (h:m am/pm): ", "h:m tt");
+            var miles = AnsiConsole.Ask<double>("Distance (in miles): ");
+            RunController.AddSession(date, start, end, miles);
+            AnsiConsole.Markup("\n[green]Record added[/]\nPress [blue]<enter>[/]");
+            Console.ReadLine();
+        }
+    }
+}
